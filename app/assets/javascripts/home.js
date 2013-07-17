0ffdcd34394f97b762d7bc4886ddec74521df2fb
tomgalpin <tomgalpin@gmail.com>
2013-07-17 00:12:11 -0700

    jquery functionality on weekdays added

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,70 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+  var remove_active = function() {
+    $('#week div').removeClass('active');
+  };
+
+  // var add_active = function() {
+  //   $.addClass('active');
+  // };
+
+  var change_weekday = function(day) {
+    $('#week').removeClass().addClass(day);
+  };
+
+  // var click_day = function(day) {
+  //   $('#day').on('click', function() {
+  //     change_weekday('day');
+  //     remove_active();
+  //     $('#day').addClass('active');
+  //   });
+  // };
+
+  ///////////////////////////////////////////
+
+  $('#monday').on('click', function() {
+    change_weekday('monday');
+    remove_active();
+    $('#monday').addClass('active');
+  });
+
+  $('#tuesday').on('click', function() {
+    change_weekday('tuesday');
+    remove_active();
+    $('#tuesday').addClass('active');
+  });
+
+  $('#wednesday').on('click', function() {
+    change_weekday('wednesday');
+    remove_active();
+    $('#wednesday').addClass('active');
+  });
+
+  $('#thursday').on('click', function() {
+    change_weekday('thursday');
+    remove_active();
+    $('#thursday').addClass('active');
+  });
+
+  $('#friday').on('click', function() {
+    change_weekday('friday');
+    remove_active();
+    $('#friday').addClass('active');
+  });
+
+  $('#saturday').on('click', function() {
+    change_weekday('saturday');
+    remove_active();
+    $('#saturday').addClass('active');
+  });
+
+  $('#sunday').on('click', function() {
+    change_weekday('sunday');
+    remove_active();
+    $('#sunday').addClass('active');
+  });
+
+
+
+
+}); // end
