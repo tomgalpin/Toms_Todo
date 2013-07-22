ba73018c71a0fed32efcd2150343a867253e930d
tomgalpin <tomgalpin@gmail.com>
2013-07-22 09:31:19 -0700

    Heroku fix

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module ProjectTwo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent Rails from loading App when precompiling to avoid Heroku errors.
+    config.assets.initialize_on_precompile = false
   end
 end
