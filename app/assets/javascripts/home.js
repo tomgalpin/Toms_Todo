d0fd2fdfad073cd7b0a567f35e54f4c1741a39f8
tomgalpin <tomgalpin@gmail.com>
2013-09-09 13:59:24 -0700

    fixing changes still

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,86 +1,94 @@
 $(document).ready(function(){
-  window.container = $('#week');
-  var days = $('.day');
-  window.card_class = $('.day').children(':first');
-  var flip_icon = $('#flip_icon');
-  var flip_back_icon = $('#flip_back_icon');
+  container = $('#week');
+  var large = $('.large');
+  var face_front_large = large.children(':first');
+  //card_class = $('.day').children(':first');
+  var flip_icon = $('.flip_icon');
+  var flip_back_icon = $('.flip_back_icon');
   var check = $('.check_icon');
   var delete_icon = $('.delete_icon');
   var add_task_icon = $('.add_task_icon');
-  var red_icon = $('#red_theme_button');
-  var black_icon = $('#black_theme_button');
-  var gray_icon = $('#gray_theme_button');
-  var jony_icon = $('#jony_ive_theme_button');
+  var red_icon = $('.red_theme_button');
+  var black_icon = $('.black_theme_button');
+  var gray_icon = $('.gray_theme_button');
+  var jony_icon = $('.jony_ive_theme_button');
   var focused;
 
-  // show flip_icon button
-  // var show_button = function(this_div) {
-  //   $(this_div).prepend(flip_icon);
-  // };
-
   // add class 'flipped' to card (flips div)
-
-  window.initialize_flipcard = function() {
+  var initialize_flipcard = function() {
     $("body").off('click');
-    $("body").on('click', "#flip_icon", function(){
-        card_class.addClass('flipped');
-      });
-
-    // remove class 'flipped' to card to flip back
-      container.off('click');
-      container.on('click', "#flip_back_icon", function() {
-        card_class.removeClass('flipped');
-      });
-
-      $( "#sortable" ).sortable();
-      $( "#sortable" ).disableSelection();
-
+    $("body").on('click', ".flip_icon", function(){
+       face_front_large.addClass('flipped');
+    });
+    $( ".sortable" ).sortable();
+    $( ".sortable" ).disableSelection();
   };
 
-  window.initialize_flipcard();
+  initialize_flipcard();
+
+
+  // var initialize_flipcard = function() {
+
+  //   $("body").off('click');
+  //   $("body").on('click', ".flip_icon", function(){
+  //       card_class.addClass('flipped');
+  //     });
+
+  //   // remove class 'flipped' to card to flip back
+  //     container.off('click');
+  //     container.on('click', ".flip_back_icon", function() {
+  //       card_class.removeClass('flipped');
+  //     });
+
+  //     $( ".sortable" ).sortable();
+  //     $( ".sortable" ).disableSelection();
+
+  // };
+
+
 
   // 'previous_day' passed in from controller
   // renders the form partial 'big_list'
-  var render_forms = function(this_div, previous_day) {
-    var day_id = $(this_div).attr('id');
-    var settings = {
-      type: "post",
-      dataType: "script",
-      url: "/switch_day",
-      data: { selected_day_of_week: day_id,
-        previous_day_of_week: previous_day
-      }
-    };
-    $.ajax(settings);
-  };
+  // var render_forms = function(this_div, previous_day) {
+  //   var day_id = $(this_div).attr('id');
+  //   var settings = {
+  //     type: "post",
+  //     dataType: "script",
+  //     url: "/switch_day",
+  //     data: { selected_day_of_week: day_id,
+  //       previous_day_of_week: previous_day
+  //     }
+  //   };
+  //   $.ajax(settings);
+  // };
 
-  // from click change days
-  var day_changer = function() {
-    var day_div = $(this);
+  // // from click change days
+  // var day_changer = function() {
+  //   var day_div = $(this);
 
-    if(day_div.hasClass("active") === false) {
-      var day_id = day_div.attr('id');
+  //   if(day_div.hasClass("active") === false) {
+  //     var day_id = day_div.attr('id');
 
-      var previous_active_day = $('.active').attr('id');
+  //     var previous_active_day = $('.active').attr('id');
 
-      // 1) remove the day class from week
-      container.removeClass().addClass(day_id);
-      // call ajax to render forms:
-      render_forms(day_div, previous_active_day);
+  //     // 1) remove the day class from week
+  //     container.removeClass().addClass(day_id);
+  //     // call ajax to render forms:
+  //     render_forms(day_div, previous_active_day);
 
 
-    }
+  //   }
 
-  };
+  // };
 
-  // initialize
-  var initialize = function() {
-    for (i=0; i<=days.length; i++) {
-      $(days[i]).on('click', day_changer);
-    }
-  };
+  // // initialize
+  // var initialize = function() {
+  //   for (i=0; i<=days.length; i++) {
+  //     $(days[i]).on('click', day_changer);
+  //   }
+  // };
 
-  initialize();
+  // initialize();
 
   // focuses on an event of the form submission;
   // prevents the default of the form submission
